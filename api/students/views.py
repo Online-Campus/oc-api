1a7fd66f52de5d6a43ad52abd1a85a9601263b96
samagragupta <samagraguptajpr@gmail.com>
2019-09-22 08:44:25 +0530

    Spelling

--- api/students/views.py
+++ api/students/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
-from .serializers import UserSearializer
+from .serializers import UserSerializer
 
 # Create your views here.
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSearializer
+    serializer_class = UserSerializer
